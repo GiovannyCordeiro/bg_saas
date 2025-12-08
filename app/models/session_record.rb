05457f0ce2e824b29453759dc7a371dc9c07de17
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-12-08 14:59:40 -0300

    test: session_record

--- app/models/session_record.rb
+++ app/models/session_record.rb
@@ -1,2 +1,10 @@
 class SessionRecord < ApplicationRecord
+  validates :title, presence: true, absence: true
+  validate :title_is_str
+
+  def title_is_str
+    unless title.is_a?(String)
+      errors.add(:title, "This atribute neeeds to string!")
+    end
+  end
 end
--- test/models/session_record_test.rb
+++ test/models/session_record_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class SessionRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @session_record = SessionRecord.new(
+      title: true,
+      subtitle: "Uma jogatina muito legal",
+      description: "Uma tarde muito agradavel com a familia",
+      session_duration: 3.3
+    )
+  end
+
+  test "Invalid title" do
+    @session_record.title = true
+    assert_not @session_record.valid?
+  end
+
+  test "Invalid session duration" do
+    @session_record.session_duration = "3.3"
+    assert_not @session_record.valid?
+  end
 end
