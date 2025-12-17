0e877d94680cdf940874b777b37e7538f27a015d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-12-17 13:01:49 -0300

    feat: adding section posts and filters

--- app/controllers/session_records_controller.rb
+++ app/controllers/session_records_controller.rb
@@ -1,3 +1,9 @@
 class SessionRecordsController < ApplicationController
-  def index; end
+  def index
+    @filters = {
+      todos: { label: "Todos", icon: "❤️", color: "pink" },
+      familia: { label: "Família", icon: "❤️", color: "pink" },
+      amigos: { label: "Amigos", icon: "👥", color: "blue" }
+    }
+  end
 end
