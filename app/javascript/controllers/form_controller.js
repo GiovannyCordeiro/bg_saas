64d428979ebe3088c1d774dde55878580c3b7211
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-01-17 21:33:12 -0300

    refact: chaning div for button in div player

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -88,14 +88,14 @@ export default class extends Controller {
   updateDisplayPlayerNames(){
     const listPlayersNameHTML = [];
     playersName.forEach(name => {
-      const divPlayer = `<div type="button" data-name="${name}" data-action="click->form#removePlayerName" class="inline-flex items-center gap-2 bg-green-200 text-gray-700 px-4 py-2 rounded-full cursor-pointer">
+      const divPlayer = `<button type="button" data-name="${name}" data-action="click->form#removePlayerName" class="inline-flex items-center gap-2 bg-green-200 text-gray-700 px-4 py-2 rounded-full cursor-pointer">
               <p class="text-sm font-medium">${name}</p>
               <div class="text-gray-600 hover:text-gray-800">
                 <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
                   </svg>
               </div>
-            </div>`;
+            </button>`;
       listPlayersNameHTML.push(divPlayer);
     });
 
