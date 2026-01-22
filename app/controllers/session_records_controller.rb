583256a1acd5c8755800979d1a30727277ea04e9
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-01-22 15:56:50 -0300

    feat: save the all informations about sessions

--- app/controllers/session_records_controller.rb
+++ app/controllers/session_records_controller.rb
@@ -21,15 +21,26 @@ class SessionRecordsController < ApplicationController
     basic_session_params = session_record_params.except(:list_session_player, :list_session_game)
     @session_record = SessionRecord.new(basic_session_params)
 
-    # if @session_record.save
-    #   redirect_to session_records_path
-    # else
-    #   puts "Erros: #{@session_record.errors.full_messages}"
-    #   render :index
-    # end
+    if @session_record.save
+      redirect_to session_records_path
+    else
+      puts "Erros: #{@session_record.errors.full_messages}"
+      render :index
+    end
 
-    list_player_params = session_record_params.slice(:list_session_player)
-    puts "List players: #{list_player_params}"
+    # Salvando jogadores e referencia da sessao (2 acoplamentos, tirar isso depois)
+    session_record_params[:list_session_player].each do |player_name|
+      saved_player = Player.create(name: player_name)
+      @session_record.list_session_player.create(player: saved_player)
+    end
+
+    # Salvando nome do jogo e referencia para sessao (2 acoplamento, tirar depois e colocar seus repectivos testes)
+    session_record_params[:list_session_game].each do |game_name|
+      game_saved = Game.create(title: game_name)
+      @session_record.list_session_game.create(game: game_saved)
+    end
+
+
 
     # puts "Lists players: #{list_session_player_params}"E
   end
